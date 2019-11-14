271ca70d0b80a167c40614c810b78fb0fbc895e8
Bhumanyu Goyal <42897290+bhu800@users.noreply.github.com>
2019-11-15 04:11:14 +0530

    Update result.php

--- result.php
+++ result.php
@@ -1,6 +1,6 @@
 <?php
 //enter your username passwd here in place of dipanshu, dipanshu23 respectively.
-    $conn = mysqli_connect('localhost','dipanshu','dipanshu23','project');
+    $conn = mysqli_connect('localhost','root','','MINIPROJ');
     $from_date = $_POST['from_date'];
     $in_date = new DateTime($from_date . " 01:00:00");
 
@@ -8,7 +8,7 @@
     $fin_date = new DateTime($to_date . " 23:00:01");
 
     $field = $_POST['field'];
-    $query = "SELECT * FROM pollution_data";
+    $query = "SELECT * FROM POLLUTION_DATA2";
     $result = $conn->query($query);
     $maximum = 0.00;
     $minimum = 10000.00;
@@ -48,3 +48,104 @@
     echo $hint;
 
 ?>
+
+<?php
+$con=mysqli_connect("localhost","root","","MINIPROJ");
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$from_date = $_POST['from_date'];
+$in_date = new DateTime($from_date . " 01:00:00");
+
+
+$to_date = $_POST['to_date'];
+$fin_date = new DateTime($to_date . " 23:00:01");
+
+$field = $_POST['field'];
+
+$params = array("TIMESTAMP","CO","H","NO2","O3","P","PM_10","PM_1_0","PM_2_5","SO2","T","WS");
+
+
+// print_r(explode("-",$from_date));
+
+$A = array();
+
+// $a = array_search($field, $params);
+
+function Search($value, $array) 
+{ 
+    return(array_search($value, $array,false)); 
+} 
+// echo "***".$field."***";
+// print_r(Search($field, $params));
+
+$sql="SELECT * FROM POLLUTION_DATA2";
+
+if ($result=mysqli_query($con,$sql))
+  {
+  // Fetch one and one row
+  while ($row=mysqli_fetch_row($result))
+    {
+        $date= explode(" ",$row[0])[0];
+        $mm = (int)explode('/',$date)[0];
+        $dd = (int)explode('/',$date)[1];
+        $yy = (int)explode('/',$date)[2];
+        $date = new DateTime(strval($yy) . "-" . strval($mm) . "-" . strval($dd) . " 12:00:00");
+        if($date>$in_date && $date<$fin_date)
+        {
+            $A[] = $row[array_search($field, $params)];
+        }
+    }
+  // Free result set
+  mysqli_free_result($result);
+}
+
+// print_r($A);
+
+mysqli_close($con);
+
+?> 
+
+<!DOCTYPE HTML>
+<html>
+<head> 
+
+<script>
+    var ff= <?php echo json_encode($A);?>;
+    var params= <?php echo json_encode($params);?>;
+    var field = "<?php echo $field; ?>";
+    var obj=[];
+    ff.forEach(ll=>{
+        obj.push({y:parseFloat(ll)});
+    })
+window.onload = function () {
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	theme: "light2",
+	title:{
+		text: "Line Plot : " + field
+	},
+	axisY:{
+		includeZero: false
+	},
+	data: [{        
+		type: "line",       
+		dataPoints: obj,
+	}]
+});
+chart.render();
+
+}
+
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+
+</body>
+</html>
